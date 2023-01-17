307aac22d81edaa4a67391c15fee8c3c7d789e99
Goldenstars0607 <alexdeneve@hotmail.be>
2023-01-17 12:13:56 +0100

    repository

--- src/main/java/be/heh/projet_test/port/in/TacosListUseCase.java
+++ src/main/java/be/heh/projet_test/port/in/TacosListUseCase.java
@@ -1,4 +1,11 @@
 package be.heh.projet_test.port.in;
 
+import be.heh.projet_test.model.Tacos;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface TacosListUseCase {
+    List<Tacos> getTacosList();
 }
