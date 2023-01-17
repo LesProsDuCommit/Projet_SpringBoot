124998a8d8e6f465eca79b671d626ecd88cc8439
Goldenstars0607 <alexdeneve@hotmail.be>
2023-01-17 11:50:23 +0100

    Clean project

---
+++ src/main/java/be/heh/projet_test/MainApplication.java
@@ -0,0 +1,13 @@
+package be.heh.projet_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/heh/projet_test/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.projet_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
