105af62a7bffdd405875891d6016c17f8aae212e
Goldenstars0607 <alexdeneve@hotmail.be>
2023-01-17 12:11:55 +0100

    getTacos Controller

--- src/main/java/be/heh/projet_test/adaptater/in/TacosController.java
+++ src/main/java/be/heh/projet_test/adaptater/in/TacosController.java
@@ -1,10 +1,24 @@
 package be.heh.projet_test.adaptater.in;
 
+import be.heh.projet_test.model.Tacos;
+import be.heh.projet_test.port.in.TacosListUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class TacosController {
+    private final TacosListUseCase tacosListUseCase;
+    private List<Tacos> tacos;
 
+    @GetMapping("/")
+    public String tacosList(Model model){
+        tacos = tacosListUseCase.getTacosList();
+        model.addAttribute("tacos", tacos);
+        return "index";
+    }
 }
--- src/main/java/be/heh/projet_test/adaptater/out/TacosRepository.java
+++ src/main/java/be/heh/projet_test/adaptater/out/TacosRepository.java
@@ -1,4 +1,10 @@
 package be.heh.projet_test.adaptater.out;
 
-public interface TacosRepository {
+import be.heh.projet_test.model.Tacos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TacosRepository extends JpaRepository<TacosJpaEntity, Integer> {
+
 }
